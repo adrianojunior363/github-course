fde5a737d9c6ea3533415630cbf61fc476ab7f29
Adriano Junior <adrianojunior363@gmail.com>
2020-01-12 21:09:42 -0300

    the game add

---
+++ thegame.py
@@ -0,0 +1,23 @@
+import sys
+print("Bem vindo ao jogo");
+ 
+nome= input('Digite o seu nome: ')
+resposta1="sim"
+resposta2="não"
+ 
+print("Gostei do seu nome %s" % nome)
+ 
+print("Olá %s" % nome, "eu quero jogar um jogo")
+ 
+print('Eu sou o dollynho, seu amiguinho, vamos jogar? hauahhauha ')
+ 
+print('Você manja de linux? ' )
+ 
+resposta1 = input('Digite sim ou não: ')
+if resposta1 == ("sim"):
+    print("Parabéns, próxima pergunta")
+ 
+else:
+    resposta2 > ("não")
+    print("Game Over")
+    sys.exit(0)
